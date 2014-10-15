13ba7886a5a608c8cdbe3a195fc485fce75fb849
Nigel Carter <exarmaster@yahoo.com>
2014-10-14 22:01:15 -0400

    Removed unnecessary variable from Main.java.

--- src/ClaspBackend/Main.java
+++ src/ClaspBackend/Main.java
@@ -3,12 +3,10 @@ package ClaspBackend;
 import ClaspUI.MainUI;
 
 public final class Main {
-	
-	public static MainUI claspWindow;
-	
+		
 	public static void main(String[] args) {
 		
-		claspWindow = new MainUI();
+		new MainUI();
 		
 	}
 	
