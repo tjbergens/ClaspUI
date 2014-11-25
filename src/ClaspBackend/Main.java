9eb26a85aca6cd62bc837fa201191b83900038e4
Mathew Church <Matchurch@knights.ucf.edu>
2014-11-25 12:21:05 -0500

    more comments

--- src/ClaspBackend/Main.java
+++ src/ClaspBackend/Main.java
@@ -5,7 +5,7 @@ import ClaspUI.MainUI;
 public final class Main {
 	
 	public static MainUI mainUI;
-
+	//Start the program
     public static void main(String[] args) {
 
         mainUI = new MainUI();
