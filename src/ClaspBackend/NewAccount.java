201b5a726b03bc95c88f2033d286382b6ad6effd
Mathew Church <Matchurch@knights.ucf.edu>
2014-11-25 12:22:23 -0500

    more comments

--- src/ClaspBackend/NewAccount.java
+++ src/ClaspBackend/NewAccount.java
@@ -3,12 +3,12 @@ package ClaspBackend;
 // New Account that will be sent to web service.
 public class NewAccount {
 
-    //I guessed that these are all the attributes of a subaccount
+    //attributes of a password account
     public String accountName;
-    public String userName; //says unused but used in constructor
-    public String password; //says unused but used in constructor
+    public String userName; 
+    public String password; 
 
-
+    //Constructor instantiates all attributes upon creation
     public NewAccount(String accountName, String userName, String password) {
         this.accountName = accountName;
         this.userName = userName;
